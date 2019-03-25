69f971a3ed78a998db8ef95d13131fa255389ea1
RNicklasR <rnicklasr@gmail.com>
2019-03-25 20:06:45 +0100

    Update logic_gates_tb.v

--- Projects/ComputerVision/Basics/LogicGates/logic_gates_tb.v
+++ Projects/ComputerVision/Basics/LogicGates/logic_gates_tb.v
@@ -2,14 +2,16 @@
 
 module logic_gates_tb;
   	reg A,B;
-  	wire O;
+  	wire O,O2,O3;
   
   logic_gates test_circuit (
     .A(A),
     .B(B),
-    .O(O)
+    .O(O),
+    .O2(O2),
+    .O3(O3)
   );
-  
+
   
   initial
     begin
@@ -23,13 +25,13 @@ module logic_gates_tb;
       	assign B=0;
 
       	#10 assign A=1;
-      	#10 assign A=0;      
       	#10 assign B=1;      
+      	#10 assign A=0;      
     
       
       	#10 assign A=1;      
       	#10 assign B=0;           
-      		assign A=0;           
+      		  assign A=0;           
       
       $finish;
     end
