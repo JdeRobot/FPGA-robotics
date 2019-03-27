310d6f76fc0e41c53933ce09713614c2aec80e80
RNicklasR <rnicklasr@gmail.com>
2019-03-27 23:02:29 +0100

    Update hex_to_rgb.v

--- Projects/ComputerVision/Basics/hex_to_rgb.v
+++ Projects/ComputerVision/Basics/hex_to_rgb.v
@@ -1,10 +1,10 @@
-module image (
+module hex_to_rgb (
                 input  wire clk,            // System clock.
                 input  wire [9:0] x_img,    // X position in image.
                 input  wire [9:0] y_img,    // Y position in image.
-                output reg  [7:0] Rp,             // Pixel (B&W) in x and y positon.
-                output reg  [7:0] Gp,             // Pixel (B&W) in x and y positon.
-                output reg  [7:0] Bp             // Pixel (B&W) in x and y positon.
+                output reg  [7:0] Rp,             // Pixel (R) in x and y positon.
+                output reg  [7:0] Gp,             // Pixel (G) in x and y positon.
+                output reg  [7:0] Bp             // Pixel (B) in x and y positon.
              );
 
     // Image in HEX.
@@ -13,7 +13,7 @@ module image (
     // Width and height image.
     parameter WIDTH = 32;
     parameter HEIGHT = 32;
-    parameter sizeOfLengthReal = 1024;   // image data : bytes: 1024 32 * 32
+    parameter sizeOfLengthReal = 3073;   // image data : bytes: 3073 32 * 32 * 3
 
     // Memory
     reg [7 : 0] img_storage [0 : sizeOfLengthReal-1];// memory to store  8-bit data image
@@ -29,9 +29,9 @@ module image (
     always @(posedge clk) begin
 
         if ((x_img >= 0) && (x_img < WIDTH ) && (y_img >= 0) && (y_img < HEIGHT))
-            Rp = img_storage[WIDTH*3*(HEIGHT-y_img-1)+3*x_img+0];
-            Gp = img_storage[WIDTH*3*(HEIGHT-y_img-1)+3*x_img+1];
-            Bp = img_storage[WIDTH*3*(HEIGHT-y_img-1)+3*x_img+2];
+          Rp = img_storage[WIDTH*3*(HEIGHT-y_img-1)+3*x_img+0];
+          Gp = img_storage[WIDTH*3*(HEIGHT-y_img-1)+3*x_img+1];
+          Bp = img_storage[WIDTH*3*(HEIGHT-y_img-1)+3*x_img+2];
      /*     else
             Rp = 8'b00000000;
             Gp = 8'b00000000;
