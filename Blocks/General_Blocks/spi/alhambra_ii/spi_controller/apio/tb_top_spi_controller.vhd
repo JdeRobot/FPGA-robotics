e967e2cd29d22f276b0c1bbb58c80748d9dc7f90
Felipe Machado <felipe.msz@gmail.com>
2021-07-30 21:48:31 +0200

    Add GET_MOTOR_TICKS to SPI FPGA-GoPiGo interface
    It has just only been tested by simulation

--- Blocks/General_Blocks/spi/alhambra_ii/spi_controller/apio/tb_top_spi_controller.vhd
+++ Blocks/General_Blocks/spi/alhambra_ii/spi_controller/apio/tb_top_spi_controller.vhd
@@ -52,6 +52,17 @@ ARCHITECTURE behavior OF tb_top_spi_controller IS
          led_eye_rght_rgb_i : IN  std_logic_vector(23 downto 0);
          led_blink_left_rgb_i : IN  std_logic_vector(23 downto 0);
          led_blink_rght_rgb_i : IN  std_logic_vector(23 downto 0);
+
+         get_motor_ticks_left_i : in std_logic;  -- get left motor ticks 
+         get_motor_ticks_rght_i : in std_logic;  -- get right motor ticks
+         -- left motor ticks are ready (one clk)
+         motor_ticks_left_rdy_o : out std_logic;
+         -- right motor ticks are ready (one clk)
+         motor_ticks_rght_rdy_o : out std_logic;
+         -- motor ticks value are 32 bits in 2's complement
+         motor_ticks_left_o : out std_logic_vector(31 downto 0);-- left motor
+         motor_ticks_rght_o : out std_logic_vector(31 downto 0);-- right motor
+
          leds : OUT  std_logic_vector(7 downto 0);
          sclk_o : OUT  std_logic;
          miso_i : IN  std_logic;
@@ -72,11 +83,23 @@ ARCHITECTURE behavior OF tb_top_spi_controller IS
    signal motor_dps_rght_i : std_logic_vector(15 downto 0) := (others => '0');
    signal led_eye_left_rgb_i : std_logic_vector(23 downto 0) := (others => '0');
    signal led_eye_rght_rgb_i : std_logic_vector(23 downto 0) := (others => '0');
-   signal led_blink_left_rgb_i : std_logic_vector(23 downto 0) := (others => '0');
-   signal led_blink_rght_rgb_i : std_logic_vector(23 downto 0) := (others => '0');
+   signal led_blink_left_rgb_i:std_logic_vector(23 downto 0) := (others => '0');
+   signal led_blink_rght_rgb_i:std_logic_vector(23 downto 0) := (others => '0');
+
+   signal get_motor_ticks_left_i : std_logic := '0';-- get left motor ticks 
+   signal get_motor_ticks_rght_i : std_logic := '0';-- get right motor ticks
+
    signal miso_i : std_logic := '0';
 
  	--Outputs
+   -- left motor ticks are ready (one clk)
+   signal motor_ticks_left_rdy_o : std_logic;
+   -- right motor ticks are ready (one clk)
+   signal motor_ticks_rght_rdy_o : std_logic;
+   -- motor ticks value are 32 bits in 2's complement
+   signal motor_ticks_left_o : std_logic_vector(31 downto 0);-- left motor
+   signal motor_ticks_rght_o : std_logic_vector(31 downto 0);-- right motor
+
    signal leds : std_logic_vector(7 downto 0);
    signal sclk_o : std_logic;
    signal mosi_o : std_logic;
@@ -101,8 +124,17 @@ BEGIN
           led_eye_rght_rgb_i => led_eye_rght_rgb_i,
           led_blink_left_rgb_i => led_blink_left_rgb_i,
           led_blink_rght_rgb_i => led_blink_rght_rgb_i,
+
+          get_motor_ticks_left_i => get_motor_ticks_left_i,
+          get_motor_ticks_rght_i => get_motor_ticks_rght_i,
+          motor_ticks_left_rdy_o => motor_ticks_left_rdy_o,
+          motor_ticks_rght_rdy_o => motor_ticks_rght_rdy_o,
+          motor_ticks_left_o     => motor_ticks_left_o,
+          motor_ticks_rght_o     => motor_ticks_rght_o,
+
           leds => leds,
           sclk_o => sclk_o,
+
           miso_i => miso_i,
           mosi_o => mosi_o,
           spi_ss_n => spi_ss_n,
@@ -112,10 +144,10 @@ BEGIN
    -- Clock process definitions
    clk_process :process
    begin
-		clk <= '0';
-		wait for clk_period/2;
-		clk <= '1';
-		wait for clk_period/2;
+      clk <= '0';
+      wait for clk_period/2;
+      clk <= '1';
+      wait for clk_period/2;
    end process;
  
 
@@ -126,20 +158,99 @@ BEGIN
       wait for 34 ns;	
       rst <= '0';
       wait for clk_period*1000;
-		led_eye_left_rgb_i <= x"FFAACC"; --"11111111_10101010_11001100";
+      led_eye_left_rgb_i <= x"FFAACC"; --"11111111_10101010_11001100";
       wait for clk_period*1000;
-		led_eye_rght_rgb_i <= x"00AAFF"; --"00000000_10101010_11111111";
+      led_eye_rght_rgb_i <= x"00AAFF"; --"00000000_10101010_11111111";
       wait for clk_period*10000;
-		led_eye_left_rgb_i <= x"7FAACC"; -- "01111111_10101010_11001100";
+      led_eye_left_rgb_i <= x"7FAACC"; -- "01111111_10101010_11001100";
       wait for clk_period*10000;
-		motor_pwm_left_i <= x"55"; -- "01010101";
+      motor_pwm_left_i <= x"55"; -- "01010101";
       wait for clk_period*10000;
-		motor_pwm_rght_i <= x"D5"; --"11010101";
+      motor_pwm_rght_i <= x"D5"; --"11010101";
       wait for clk_period*10000;
-		motor_dps_limit_i <= x"012C"; --"00000001_00101100";
+      motor_dps_limit_i <= x"012C"; --"00000001_00101100";
       wait for clk_period*10000;
-		led_eye_left_rgb_i <= x"1132CC"; -- "00010001_00110010_11001100";
+      led_eye_left_rgb_i <= x"1132CC"; -- "00010001_00110010_11001100";
+      wait for clk_period*10000;
+      wait until clk'event and clk= '1';
+      get_motor_ticks_left_i <= '1';   -- get left motor ticks
+      wait until clk'event and clk= '1';
+      get_motor_ticks_left_i <= '0';
+      wait for clk_period*10000;
+      wait until clk'event and clk= '1';
+      get_motor_ticks_rght_i <= '1';  -- get right motor ticks
+      wait until clk'event and clk= '1';
+      get_motor_ticks_rght_i <= '0';
+      wait;
+
       wait;
    end process;
+
+   -- simulates the gopigo MISO
+   gopigo_sim: process(spi_ss_n, sclk_o)
+     -- indicates which spi byte is being sent
+     variable spi_byte : integer range 0 to 15;
+     variable clk_up_cnt   : integer range 0 to 7;
+     variable clk_down_cnt : integer range 0 to 7;
+   begin
+     miso_i <= '0';
+     if spi_ss_n = '1' then
+       spi_byte := 0; -- not enabled, count to zero
+     else  -- enabled
+       if sclk_o'event and sclk_o='1' then -- rising edge
+         if clk_up_cnt = 7 then
+           clk_up_cnt := 0;
+         else
+           clk_up_cnt := clk_up_cnt + 1;
+         end if;
+       elsif sclk_o'event and sclk_o='0' then -- falling edge
+         if clk_down_cnt = 7 then
+           clk_down_cnt := 0;
+           spi_byte := spi_byte + 1;
+         else
+           clk_down_cnt := clk_down_cnt + 1;
+         end if;
+       end if;
+       if spi_byte = 3 then -- send gopigo acknowledgement 0xA5 1010_0101 
+         case clk_down_cnt is
+           when 0 | 2 | 5 | 7 =>
+             miso_i <= '1';
+           when others =>
+             miso_i <= '0';
+         end case;
+       -- this is to test receiving data from the slave
+       elsif spi_byte = 4 then -- byte 3: 0001_0101: 0x15 (for example)
+         case clk_down_cnt is
+           when 3 | 5 | 7 =>
+             miso_i <= '1';
+           when others =>
+             miso_i <= '0';
+         end case;
+       elsif spi_byte = 5 then -- byte 2: 1000_1001: 0x89 (for example)
+         case clk_down_cnt is
+           when 0 | 4 | 7 =>
+             miso_i <= '1';
+           when others =>
+             miso_i <= '0';
+         end case;
+       elsif spi_byte = 6 then -- byte 1: 1100_0010: 0xC2 (for example)
+         case clk_down_cnt is
+           when 0 | 1 | 6 =>
+             miso_i <= '1';
+           when others =>
+             miso_i <= '0';
+         end case;
+       elsif spi_byte = 7 then -- byte 0: 1111_1101: 0xFD (for example)
+         case clk_down_cnt is
+           when 0 | 1 | 2 | 3 | 4 | 5| 7 =>
+             miso_i <= '1';
+           when others =>
+             miso_i <= '0';
+         end case;
+       end if;
+     end if;
+   end process;
+
+     
 
 END;
--- Blocks/General_Blocks/spi/alhambra_ii/spi_controller/apio/top_spi_controller_wrp.v
+++ Blocks/General_Blocks/spi/alhambra_ii/spi_controller/apio/top_spi_controller_wrp.v
@@ -1,5 +1,29 @@
-// wrapper for top_spi_controller, in order to have less ports
+// ----------------------
+// Felipe Machado Sanchez
+// Area de Tecnologia Electronica
+// Universidad Rey Juan Carlos
+// https://github.com/felipe-m
+
+// FPGA-GoPiGo3 SPI interface
+// This is wrapper for top_spi_controller, in order to have less ports
 // These port are not used in normal operation
+
+// There are 2 kind of commands, SET and GET.
+// +SET commands establish a new value, for example motor_dps or led_brightness
+// +GET commands ask GoPiGo3 to send the value
+
+// For SET commands, this module routinely checks if input ports have changed
+// if there is any change, it will send the new value to the GoPiGo3 via SPI
+// These ports have different widths, which depend on the GoPiGo3 specifications
+
+// For GET commands, this module checks if its corresponding input is '1'
+// in that case, this module will send the reading command to de SPI
+// The input should only be '1' a clock cycle. This inputs are just one bit
+// Once the SPI command is received, it will be available in parallel at its
+// corresponding output port.
+// There will be also a output bit port that indicates that output value is
+// valid. This signal will be available for just one clock cycle
+
 
 module top_spi_controller_wrp
   #(
@@ -14,6 +38,7 @@ module top_spi_controller_wrp
   input  clk,
   input  rst,
 
+  // SET commands
   input  [15:0] motor_dps_left_i, // left motor DPS (degrees per second)
                                   // limited by motor_dps_limit_i
   input  [15:0] motor_dps_rght_i, // right motor DPS (degrees per second)
@@ -26,9 +51,21 @@ module top_spi_controller_wrp
   input  [24-1:0] led_blink_left_rgb_i,
   // led blink right rgb color: 0 to 255 each channel R[23:16] G[15:8] B[7:0]
   input  [24-1:0] led_blink_rght_rgb_i,
-  
+
+  // GET commands
+  // get left motor ticks. Active just one clock cycle
+  input     get_motor_ticks_left_i,  // get left motor ticks 
+  input     get_motor_ticks_rght_i,  // get right motor ticks
+
+  output     motor_ticks_left_rdy_o,  // left motor ticks are ready (one clk)
+  output     motor_ticks_rght_rdy_o,  // right motor ticks are ready (one clk)
+  // motor ticks value are 32 bits in 2's complement
+  output [32-1:0] motor_ticks_left_o,  // left motor ticks value
+  output [32-1:0] motor_ticks_rght_o,  // right motor ticks value
 
   output [7:0] leds,
+
+
   // SPI
   output sclk_o, 
   input  miso_i,
@@ -73,6 +110,15 @@ module top_spi_controller_wrp
     .led_eye_rght_rgb_i   (led_eye_rght_rgb_i),
     .led_blink_left_rgb_i (led_blink_left_rgb_i),
     .led_blink_rght_rgb_i (led_blink_rght_rgb_i),
+
+
+    .get_motor_ticks_left_i (get_motor_ticks_left_i),
+    .get_motor_ticks_rght_i (get_motor_ticks_rght_i),
+    .motor_ticks_left_rdy_o (motor_ticks_left_rdy_o),
+    .motor_ticks_rght_rdy_o (motor_ticks_rght_rdy_o),
+    .motor_ticks_left_o     (motor_ticks_left_o),
+    .motor_ticks_rght_o     (motor_ticks_rght_o),
+
     .sclk_o               (sclk_o), 
     .miso_i               (miso_i),
     .mosi_o               (mosi_o),
