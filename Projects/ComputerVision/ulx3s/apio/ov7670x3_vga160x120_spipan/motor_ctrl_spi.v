23095bfb6007c8a08b0c4707caa5a4db950a932d
Felipe Machado <felipe.msz@gmail.com>
2022-02-22 22:30:03 +0100

    Fix bug in waiting time for motor control

--- Projects/ComputerVision/ulx3s/apio/ov7670x3_vga160x120_spipan/motor_ctrl_spi.v
+++ Projects/ComputerVision/ulx3s/apio/ov7670x3_vga160x120_spipan/motor_ctrl_spi.v
@@ -1,8 +1,15 @@
+// the max frames per second of the camera is 30 fps, so it will be
+//  at 33.3 ms each new centroid. But it probably will be less than that.
+//  I think it is 15 fps
+//  So signal new_centroid will have this period.
+//  Considering about ~50ms, cnt to stop searching the object
+
 module motor_ctrl_spi
   #(
     parameter  nb_dps_motor = 16,
-    parameter  nb_Pctrl = 3,
-    parameter  nb_counter = 32
+    //parameter  nb_Pctrl = 3,
+    // number of bits of the counter: ~50ms x 64 ~3.2s to stop searching
+    parameter  nb_cnt = 6
   )
   (input        rst,
    input        clk,
@@ -24,10 +31,13 @@ module motor_ctrl_spi
   //wire signed [nb_Pctrl-1:0] Prgth;
   //wire signed [nb_Pctrl-1:0] Pleft;
 
-  reg lost_obj_n = 1'b1; // 0=lost; 1=looking
+  reg lost_obj_n; // 0=lost; 1=looking
 
   // Counter register
-  reg [nb_counter-1:0] counter = 0;
+  reg [nb_cnt-1:0]  cnt;
+  localparam [nb_cnt-1:0] c_end_cnt = {nb_cnt{1'b1}}; //to have all to 1
+  wire   cnt_end; // end of the count
+
 
   // Last known centroid
   reg [8-1:0] last_cent_valid;
@@ -40,6 +50,8 @@ module motor_ctrl_spi
   localparam signed [nb_dps_motor-1:0] c_vel3_neg = -c_vel3; 
   localparam signed [nb_dps_motor-1:0] c_vel2_neg = -c_vel2; 
   localparam signed [nb_dps_motor-1:0] c_vel1_neg = -c_vel1; 
+
+  wire       tracking; // 0=lost; 1=tracking object
 
   // Output assign 
   assign motor_dps_left_o = motor_dps_left;
@@ -55,9 +67,13 @@ module motor_ctrl_spi
   //assign direction = (proximity - 3'd3) * (-7'd16);
 
 
-  always @(posedge clk)
+  always @(posedge rst, posedge clk)
   begin
-    if (~enable) begin
+    if (rst) begin
+      motor_dps_left <= 0;
+      motor_dps_rght <= 0;
+    end
+    else if (~enable) begin
       motor_dps_left <= 0;
       motor_dps_rght <= 0;
     end
@@ -146,27 +162,52 @@ module motor_ctrl_spi
     end
   end
  
-  // Process to stop the searching if the counter limit is reached.
-  always @(posedge clk)
+  // Process to stop the searching if the cnt limit is reached.
+  always @(posedge rst, posedge clk)
   begin
-    if(counter > 32'h40000000) begin
+    if(rst) begin
+      lost_obj_n <= 1'b0; // it doesnt matter, not looking
+    end
+    else if(~enable) begin
+      lost_obj_n <= 1'b0; // it doesnt matter, not looking
+    end
+    else if(cnt_end) begin
       lost_obj_n <= 1'b0; // lost
-    end else begin
+    end
+    else begin
       lost_obj_n <= 1'b1; // looking
     end
   end
 
+
+  // an object is captured by the camera 
+  assign tracking = (centroid == 8'h00) ? 1'b0 : 1'b1;
+
   // Process to find the object from the last known position.
-  always @(posedge clk) begin
-    if (~enable) begin
-      counter <= 32'h00000000;
+  always @(posedge rst, posedge clk)
+  begin
+    if (rst) begin
+      last_cent_valid <= 8'h00;
+      cnt <= 0;
     end
-    else if(centroid == 8'h00) begin
-      counter <= counter + 1;
-    end else begin
-      last_cent_valid[7:0] <= centroid[7:0];
-      counter <= 32'h00000000;
+    else begin
+      if (new_centroid) begin
+        if (~tracking) begin
+          if (cnt_end) begin
+            cnt <= cnt; // keep the count, to have cnt_end == 1
+          end
+          else begin
+            cnt <= cnt + 1;
+          end
+        end
+        else begin // if tracking, count resets
+          cnt <= 0;
+          last_cent_valid <= centroid;
+        end
+      end
     end
   end
+
+  assign cnt_end = (cnt == c_end_cnt) ? 1'b1 : 1'b0;
 
 endmodule
