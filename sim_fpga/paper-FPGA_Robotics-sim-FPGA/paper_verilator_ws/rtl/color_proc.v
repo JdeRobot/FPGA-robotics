d14ef88c11f281f29ecd116ca2f4fcacf0c755d9
Felipe Machado <felipe.msz@gmail.com>
2023-10-12 14:30:05 +0100

    Adjust RTL color filter to avoid detecting wood
    Adjust RTL color filter to avoid detecting wood as red
    modified:   paper-FPGA_Robotics-sim-FPGA/paper_verilator_ws/rtl/color_proc.v

--- sim_fpga/paper-FPGA_Robotics-sim-FPGA/paper_verilator_ws/rtl/color_proc.v
+++ sim_fpga/paper-FPGA_Robotics-sim-FPGA/paper_verilator_ws/rtl/color_proc.v
@@ -212,7 +212,7 @@ module color_proc
   wire [c_nb_buf_red-1:0]  red_blu_absdif;
   wire [c_nb_buf_green-1:0] grn_blu_absdif;
   localparam  [c_nb_buf_red-1:0] c_simil_limit = 2; // limit to consider similar
-  localparam  [c_nb_buf_red-1:0] c_vdif_limit = 4; // limit to consider very different
+  localparam  [c_nb_buf_red-1:0] c_vdif_limit = 6; // limit to consider very different
 
   wire red_grn_simil, red_blu_simil, grn_blu_simil;
   wire red_grn_vdif, red_blu_vdif, grn_blu_vdif;
